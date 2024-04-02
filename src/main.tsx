3011946dc2cb521e503be20242acd00a621b085e
Vitor Cunha <vitor.dc@hotmail.com>
2024-04-02 11:20:22 -0300

    feature: Add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 // Libs
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Components
 import App from "./App.tsx";
@@ -8,8 +9,15 @@ import App from "./App.tsx";
 // Styles
 import "./styles/global.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
