558682597aa38d29878ebeaf46583514ebc1a7a7
Vitor Cunha <vitor.dc@hotmail.com>
2024-04-01 22:24:38 -0300

    feature: Avatar component

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,15 @@
+import { User } from "lucide-react";
+
+import Button from "../button";
+
+const Avatar = () => {
+  return (
+    <div className="header-button">
+      <Button>
+        <User />
+      </Button>
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ src/components/avatar/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  border: 1px solid #f1f1f1;
+  border-radius: 30px;
+`;
